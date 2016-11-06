7624584ab738d38f27203d11d02072e6493c7f44
hanbiao.xiong <hanbiao.xiong@ele.me>
2016-11-07 00:47:48 +0800

    添加search-box组件

--- src/lib/components/amap-search-box.vue
+++ src/lib/components/amap-search-box.vue
@@ -1,9 +1,93 @@
 <template>
-  <div class="el-vue-search-box-container">
-    <input type="text" v-model="keyword" @input="search">
-    <div v-for="tip in tips">{{tip.name}}</div>
+  <div class="el-vue-search-box-container"
+       @keydown.up="selectTip('up')"
+       @keydown.down="selectTip('down')">
+    <div class="search-box-wrapper">
+      <input type="text"
+             v-model="keyword"
+             @keyup.enter="search"
+             @input="autoComplete">
+      <a class="search-btn" @click="search" >搜索</a>
+    </div>
+    <div class="search-tips">
+      <ul>
+        <li v-for="(tip, index) in tips"
+            @click="changeTip(tip)"
+            @mouseover="selectedTip=index"
+            :class="{'autocomplete-selected': index === selectedTip}">{{tip.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
+<style lang="scss">
+  .el-vue-search-box-container {
+    position: relative;
+    width: 360px;
+    height: 45px;
+    background: #fff;
+    box-shadow: 0 2px 2px rgba(0,0,0,.15);
+    border-radius: 2px 3px 3px 2px;
+    z-index: 10;
+
+    .search-box-wrapper {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+
+      input {
+        flex: 1;
+        height: 20px;
+        line-height: 20px;
+        letter-spacing: .5px;
+        font-size: 14px;
+        text-indent: 10px;
+        box-sizing: border-box;
+        border: none;
+        outline: none;
+      }
+
+      .search-btn {
+        width: 45px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: transparent;
+        cursor: pointer;
+      }
+    }
+
+    .search-tips {
+      position: absolute;
+      top: 100%;
+      border: 1px solid #dbdbdb;
+      background: #FFF;
+      overflow: auto;
+
+      ul {
+        padding: 0;
+        margin: 0;
+
+        li {
+          height: 40px;
+          line-height: 40px;
+          box-shadow: 0 1px 1px rgba(0,0,0,.1);
+          padding: 0 10px;
+          cursor: pointer;
+
+          &.autocomplete-selected {
+            background: #eee;
+          }
+        }
+      }
+    }
+  }
+</style>
 <script>
 import RegisterComponentMixin from '../mixins/register-component-mixin';
 import {lazyAMapApiLoaderInstance} from '../services/injected-amap-api-instance';
@@ -11,19 +95,23 @@ import {lazyAMapApiLoaderInstance} from '../services/injected-amap-api-instance'
 export default {
   name: 'el-amap-search-box',
   mixins: [RegisterComponentMixin],
-  props: ['city', 'type', 'dataType', 'citylimit', 'input'],
+  props: ['mapConfig', 'onSearchResult'],
   data() {
     return {
       keyword: '',
-      tips: []
+      tips: [],
+      selectedTip: -1
     };
   },
   beforeCreate() {
     this._loadApiPromise = lazyAMapApiLoaderInstance.load();
     this._loadApiPromise.then(() => {
       let options = this.getOptions();
-      this.$searchOptions = options;
-      this.$auto = new AMap.Autocomplete(options);
+      let {mapConfig, onSearchResult} = options;
+//      this._searchOptions = mapConfig;
+      this._autoComplete = new AMap.Autocomplete(mapConfig);
+      this._placeSearch = new AMap.PlaceSearch(mapConfig);
+      this._onSearchResult = onSearchResult;
     });
   },
   methods: {
@@ -35,12 +123,30 @@ export default {
       }
       return tmpOptions;
     },
-    search() {
-      this.$auto.search(this.keyword, (status, result) => {
+    autoComplete() {
+      this._autoComplete.search(this.keyword, (status, result) => {
         if (status === 'complete') {
           this.tips = result.tips;
         }
       });
+    },
+    search() {
+      this.tips = [];
+      console.log('search', this.keyword);
+      this._placeSearch.search(this.keyword, this._onSearchResult);
+    },
+    changeTip(tip) {
+      this.keyword = tip.name;
+      this.search();
+    },
+    selectTip(type) {
+      if (type === 'up' && this.selectedTip > 0) {
+        this.selectedTip -= 1;
+        this.keyword = this.tips[this.selectedTip].name;
+      } else if (type === 'down' && this.selectedTip + 1 < this.tips.length) {
+        this.selectedTip += 1;
+        this.keyword = this.tips[this.selectedTip].name;
+      }
     }
   }
 };
