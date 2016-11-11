02916b84475445286acfbd9fc394a64a1f39c084
silas <369694143@qq.com>
2016-11-11 17:38:45 +0800

    add marker functions

--- src/lib/components/amap-marker.vue
+++ src/lib/components/amap-marker.vue
@@ -5,28 +5,59 @@
   export default {
     name: 'el-amap-marker',
     mixins: [RegisterComponentMixin],
-    props: ['lng', 'lat', 'draggable', 'options', 'position'],
-    destoryed() {
-
-    },
+    props: ['markerOptions'],
     methods: {
       initComponent() {
-        if (!this.$map) throw new Error('map instance not initaled');
-        let markerOptions = this.getOptions();
-        if (markerOptions.position && markerOptions.position.length) {
-          markerOptions.position = new AMap.LngLat(markerOptions.position[0], markerOptions.position[1]);
+        if (!this.$map) throw new Error('map instance not initialized');
+        let options = this.getOptions();
+        if (options.position && Array.isArray(options.position)) {
+          options.position = new AMap.LngLat(options.position[0], options.position[1]);
         }
-        markerOptions.map = this.$map;
-        this.$marker = new AMap.Marker(markerOptions);
-        this.$markerOptions = markerOptions;
+        if (options.offset && Array.isArray(options.offset)) {
+          options.offset = new AMap.Pixel(options.offset[0], options.offset[1]);
+        }
+        options.map = this.$map;
+        this.$marker = new AMap.Marker(options);
+        this.$markerOptions = options;
+        ['map', 'offset', 'position', 'label', 'icon', 'content', 'shadow', 'shape', 'extData', 'angle', 'zIndex', 'clickable', 'draggable', 'top', 'animation', 'cursor', 'title'].forEach(item => {
+          this.generateSetFn(item);
+        });
+        ['map', 'offset', 'position', 'label', 'icon', 'content', 'shadow', 'shape', 'extData', 'angle', 'clickable', 'draggable', 'top', 'animation', 'title'].forEach(item => {
+          this.generateGetFn(item);
+        });
+      },
+      generateSetFn(name) {
+        var fn = 'set' + (name.substring(0, 1).toUpperCase() + name.substring(1));
+        this[fn] = function(value) {
+          this.$map[fn].call(this[fn], value);
+        };
+      },
+      generateGetFn(name) {
+        var fn = 'get' + (name.substring(0, 1).toUpperCase() + name.substring(1));
+        this[fn] = function() {
+          return this.$map[fn].call(this[fn]);
+        };
+      },
+      markOnAMAP(obj) {
+        if (!obj.name) return;
+        this.$marker.markOnAMAP(obj);
+      },
+      hide() {
+        this.$marker.hide();
+      },
+      show() {
+        this.$marker.show();
+      },
+      moveAlong(pointlist, speed, fn, circlable) {
+        if (!pointlist instanceof Array || !speed instanceof Number) return;
+        this.$marker.moveAlong(pointlist, speed, fn, circlable);
+      },
+      moveTo(poi, speed, fn) {
+        this.$marker.moveTo(poi, speed, fn);
+      },
+      stopMove() {
+        this.$marker.stopMove();
       }
-    },
-    setCenter() {
-
-    },
-    getCenter() {
-      if (!this.$map) throw new Error('not ready for');
-      return this.$map.getCenter();
     }
   };
 </script>
--- src/lib/managers/amap-marker-manager.js
+++ src/lib/managers/amap-marker-manager.js
@@ -1,5 +1,21 @@
 export default class MarkerManager {
   constructor() {
-    this._markers = new Map();
+    this.map = null;
+    this.offset = null;
+    this.position = null;
+    this.label = null;
+    this.icon = null;
+    this.content = null;
+    this.shadow = null;
+    this.shape = null;
+    this.extData = null;
+    this.angle = 0;
+    this.zIndex = 0;
+    this.clickable = false;
+    this.draggable = false;
+    this.top = false;
+    this.animation = '';
+    this.cursor = '';
+    this.title = '';
   }
 };
