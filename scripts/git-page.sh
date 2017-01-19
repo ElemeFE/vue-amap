60c8d9b7b96ae11f22b6ab553731d4a5990d0c52
wei.zhang <wei.zhang12@ele.me>
2017-01-19 17:02:02 +0800

    update:test-ci

--- scripts/git-page.sh
+++ scripts/git-page.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-if [ "$GITHUB_TOKEN" ]; then
+if [ "$" ]; then
   cd docs
+  rm -rf .git
   git init
-  gco -b
   git checkout -b gh-pages
   git add .
-  git -c user.name='travis' -c user.email=$NPM_EMAIL commit -m "update:docs"
-  git push -f -q https://$GITHUB_TOKEN@git@github.com:ElemeFE/vue-amap.git gh-pages &2>/dev/null
+  git -c user.name='travis' -c user.email=$NPM_EMAIL commit -m "update: docs-branch"
+  git push -f -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/ElemeFE/vue-amap.git gh-pages &2>/dev/null
   cd ..
-if
+fi
