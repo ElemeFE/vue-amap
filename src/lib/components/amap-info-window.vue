85b348a467d2665ae40aaedb50e9b0ec43bab944
zheng yu <zheng.yu@ele.me>
2017-05-24 10:37:42 +0800

    fix: fixed issue #112

--- src/lib/components/amap-info-window.vue
+++ src/lib/components/amap-info-window.vue
@@ -27,20 +27,10 @@ export default {
       handlers: {
         visible(flag) {
           // fixed Amap info-window reopen
-          let lng = 0;
-          let lat = 0;
-
-          if (this.G && this.G.position) {
-            lng = this.G.position.lng;
-            lat = this.G.position.lat;
-          } else if (this.si && this.si.position) {
-            // #90 https://github.com/ElemeFE/vue-amap/commit/6ac6bc8d51ff11c8009d056fbdd1fdb4f92aac7a
-            // i can't find si in this scope. why ?
-            lng = this.si.position.lng;
-            lat = this.si.position.lat;
+          let position = this.getPosition();
+          if (position) {
+            flag === false ? this.close() : this.open(this.G.map, [position.lng, position.lat]);
           }
-
-          flag === false ? this.close() : this.open(this.G.map, [lng, lat]);
         }
       }
     };
