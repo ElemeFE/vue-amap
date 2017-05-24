da295ad7dc81db4de3cd8e66eaaabf58e4b92bee
zheng yu <zheng.yu@ele.me>
2017-05-24 14:53:30 +0800

    Update 404 sidebar and

--- Makefile
+++ Makefile
@@ -3,11 +3,6 @@
 build:
 	@npm run build
 
-# 由于集成了 docsify 目前需要启两个服务
-# 先 make docsify
-docsify:
-	docsify serve src/docs
-
 # 然后新开一个 console
 # make dev
 dev:
--- config/index.js
+++ config/index.js
@@ -33,9 +33,7 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      '/**/*.md': 'http://localhost:3000/'
-    },
+    proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
